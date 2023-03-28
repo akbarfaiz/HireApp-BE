a475a22125c91dab4315aa779ad67443ea911233
fauzanilmi220 <fauzanilmi220@gmail.com>
2023-03-28 21:14:30 +0700

    add message CRUD

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,38 +5,14 @@ const Perusahaan = require('./perusahaanRoute')
 const Skill = require('./skillRoute')
 const Experience = require('./experienceRoute')
 const Pekerja = require('./pekerjaRoute')
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-const Chat = require('./messagesRoute')
-=======
 const Portofolio = require('./portofolioRoute')
->>>>>>> 5db94a36141cc9f6412aada6f32554b62eb19fda
-=======
-const Portofolio = require('./portofolioRoute')
->>>>>>> 5db94a36141cc9f6412aada6f32554b62eb19fda
-=======
-const Portofolio = require('./portofolioRoute')
->>>>>>> 5db94a36141cc9f6412aada6f32554b62eb19fda
 
 router.use('/users',Users)
 router.use('/company',Perusahaan)
 router.use('/skill',Skill)
 router.use('/experience',Experience)
 router.use('/pekerja', Pekerja)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-router.use('/chat', Chat)
-=======
 router.use('/portofolio', Portofolio)
->>>>>>> 5db94a36141cc9f6412aada6f32554b62eb19fda
-=======
-router.use('/portofolio', Portofolio)
->>>>>>> 5db94a36141cc9f6412aada6f32554b62eb19fda
-=======
-router.use('/portofolio', Portofolio)
->>>>>>> 5db94a36141cc9f6412aada6f32554b62eb19fda
 
 router.get('/',function(req, res) {
     res.json({ message: 'Welcome to Hire App API !!!' });   
